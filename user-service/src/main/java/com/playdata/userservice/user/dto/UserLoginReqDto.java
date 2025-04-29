129233ae17a1ecc376b430a53c60723e32de37bd
Jongkyun <hjk326333@gmail.com>
2025-04-29 16:39:43 +0900

    User Service 프로젝트 추가 및 정리

---
+++ user-service/src/main/java/com/playdata/userservice/user/dto/UserLoginReqDto.java
@@ -0,0 +1,15 @@
+package com.playdata.userservice.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserLoginReqDto {
+
+    private String email;
+    private String password;
+}
