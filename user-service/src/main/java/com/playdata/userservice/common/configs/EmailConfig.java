dd435be6f5bb99ca3e8bf871a466ea50e59091f4
Jongkyun <hjk326333@gmail.com>
2025-05-26 16:38:47 +0900

    네이버 메일을 발신용으로 사용할 때의 설정

--- user-service/src/main/java/com/playdata/userservice/common/configs/EmailConfig.java
+++ user-service/src/main/java/com/playdata/userservice/common/configs/EmailConfig.java
@@ -19,8 +19,12 @@ public class EmailConfig {
     private String password;
     @Value( "${spring.mail.properties.mail.smtp.auth}")
     private boolean auth;
-    @Value( "${spring.mail.properties.mail.smtp.starttls.enable}")
-    private String starttlsEnable;
+    @Value( "${spring.mail.properties.mail.smtp.starttls.enable}") // 구글
+    private String starttlsEnable; // 구글
+//    @Value("${spring.mail.properties.mail.smtp.ssl.enable}")  // 네이버
+//    private boolean sslEnable;
+//    @Value( "${spring.mail.properties.mail.smtp.ssl.trust}") // 네이버
+//    private String sslTrust;
 
     @Bean
     public JavaMailSender javaMailSender() {
@@ -34,7 +38,10 @@ public class EmailConfig {
         // 기타 보안 전송 및 전송 방식에 대한 사항은 Properties 객체 형태로 전달.
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", auth);
-        properties.put("mail.smtp.starttls.enable", starttlsEnable);
+        properties.put("mail.smtp.starttls.enable", starttlsEnable); // (구글)
+//        properties.put("mail.smtp.ssl.enable", sslEnable); // (네이버)
+//        properties.put("mail.smtp.ssl.trust", sslTrust); // (네이버)
+
 
         mailSender.setJavaMailProperties(properties);
 
